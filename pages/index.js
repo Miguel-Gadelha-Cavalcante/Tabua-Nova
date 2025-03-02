faaf15f9e90489a20dee7b45f29fd35bd9ab2625
Miguel Gadelha <83650029+Miguel-Gadelha-Cavalcante@users.noreply.github.com>
2025-03-02 14:22:53 -0300

    resolvendo problemas técnicos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Testes 1</h1>;
+  return (
+    <h1>
+      Eu me odeio muito e estou disposto a me acostumar com isso em prou da
+      minha educação
+    </h1>
+  );
 }
 
 export default Home;
--- pages/produtos/index.js
+++
@@ -1,5 +0,0 @@
-var r=0;
-
-const ruh (r) => {
-    return r;
-};
