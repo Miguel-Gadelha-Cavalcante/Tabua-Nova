0cf4478a4314aaea47f8787b55a23353bc585655
Miguel Gadelha <83650029+Miguel-Gadelha-Cavalcante@users.noreply.github.com>
2025-03-06 13:53:15 -0300

    Erro de português

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,10 @@
 function Home() {
   return (
     <h1>
-      Eu me odeio muito e estou disposto a me acostumar com isso em prou da
+      Eu me odeio muito e estou disposto a me acostumar com isso em prol da
       minha educação 😏
     </h1>
   );
-}
-
-function teste() {
-  return console.log("Olá, mundo!");
-}
-function teste2() {
-  return console.log("identação errada");
 }
 
 export default Home;
