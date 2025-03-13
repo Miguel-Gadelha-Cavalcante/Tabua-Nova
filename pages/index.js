1cde81999a3a8841730956f78143ed4469edb030
Miguel Gadelha <83650029+Miguel-Gadelha-Cavalcante@users.noreply.github.com>
2025-03-13 11:13:12 -0300

    Modificação da mensagem do `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Eu me odeio muito e estou disposto a me acostumar com isso em prol da
-      minha educação 😏
-    </h1>
-  );
+  return <h1>Olá, Mundo! 😏</h1>;
 }
 
 export default Home;
